2604dde81d8d8d78d0468059039ee96c892456b5
abdullah107189 <abdullah107189@gmail.com>
2024-10-04 08:28:32 +0600

    complate live time show in display

--- time.js
+++ time.js
@@ -1,2 +1,37 @@
-const date = new Date()
-console.log(date)
+const date = new Date();
+const timeCall = () => {
+    const date = new Date();
+    const hoursInput = document.getElementById('hours')
+    const minuetsInput = document.getElementById('minuets')
+    const SecondsInput = document.getElementById('Seconds')
+
+    const hours = String(date.getHours()).padStart(2, '0')
+    const minutes = String(date.getMinutes()).padStart(2, '0')
+    const seconds = String(date.getSeconds()).padStart(2, '0')
+    hoursInput.innerText = hours
+    minuetsInput.innerText = minutes
+    SecondsInput.innerText = seconds
+}
+setInterval(() => {
+    timeCall() 
+}, 1000);
+
+
+
+const year = date.getFullYear()
+const month = date.getMonth()
+const day = date.getDate()
+const week = date.getDay();
+
+const dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+const dateMonthYear = document.getElementById('date-month-year')
+dateMonthYear.innerHTML = `
+
+        <span>${String(dayName[week])}</span> 
+        <span>${String(day).padStart(2, '0')}</span> 
+        <span>${String(monthName[month])}</span> 
+        <span>${year}</span> 
+
+`
