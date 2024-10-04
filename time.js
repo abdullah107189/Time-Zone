4bc93744cda7b59667030d6c8eccfe83020c8ee7
abdullah107189 <abdullah107189@gmail.com>
2024-10-04 12:12:02 +0600

    added am/pm

--- time.js
+++ time.js
@@ -4,16 +4,23 @@ const timeCall = () => {
     const hoursInput = document.getElementById('hours')
     const minuetsInput = document.getElementById('minuets')
     const SecondsInput = document.getElementById('Seconds')
+    const amPm = document.getElementById('am-pm')
 
-    const hours = String(date.getHours()).padStart(2, '0')
+    let hours = date.getHours()
+    const setAmPm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12 || 12
+    const hoursFormat = String(hours).padStart(2, '0')
     const minutes = String(date.getMinutes()).padStart(2, '0')
     const seconds = String(date.getSeconds()).padStart(2, '0')
+
     hoursInput.innerText = hours
     minuetsInput.innerText = minutes
     SecondsInput.innerText = seconds
+    amPm.innerText = setAmPm
+
 }
 setInterval(() => {
-    timeCall() 
+    timeCall()
 }, 1000);
 
 
